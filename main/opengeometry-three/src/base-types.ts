db6e8703f72529f7a8cbaa1b91465d13c2dfda4f
Vishwajeet <manevishwajeet1@gmail.com>
2025-10-08 17:26:04 +0200

    (test) Release

--- main/opengeometry-three/src/base-types.ts
+++ main/opengeometry-three/src/base-types.ts
@@ -3,7 +3,7 @@ import {
   // Vector3 
 } from "./../../opengeometry/pkg/opengeometry";
 
-export const OPEN_GEOMETRY_THREE_VERSION = '0.0.1';
+export const OPEN_GEOMETRY_THREE_VERSION = '0.0.6';
 
 export interface OpenGeometryOptions {
   wasmURL?: string;
